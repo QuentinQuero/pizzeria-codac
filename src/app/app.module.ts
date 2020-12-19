d7556b142d4bcce94a64ddf14af1d72e72c063bb
Quentin <quentin2.quero@epitech.eu>
2020-12-19 01:27:27 +0100

    clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PizzaRowSelectionComponent } from './pizza-row-selection/pizza-row-selection.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaRowSelectionComponent,
     HomeComponent
   ],
   imports: [
