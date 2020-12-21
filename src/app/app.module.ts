892d1a73da50f12926974f06e7cf4f2a5ab07a80
Renard <mathis.renard@epitech.eu>
2020-12-21 16:13:47 +0100

    Add File Tree

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {APIInterceptor} from "./HttpInterceptor";
+import {APIInterceptor} from "./module/HttpInterceptor";
 
 @NgModule({
   declarations: [
