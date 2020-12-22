a5c5270a9e82f23226daaec208dd352d831bf567
Quentin <quentin2.quero@epitech.eu>
2020-12-22 11:05:23 +0100

    patch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {APIInterceptor} from "./HttpInterceptor";
 import { FooterComponent } from './components/footer/footer.component';
 import { CartComponent } from './cart/cart.component';
 import { FormsModule }   from '@angular/forms';
 import { PizzaComponent } from './components/pizza/pizza.component'
+import {APIInterceptor} from './module/HttpInterceptor';
 
 @NgModule({
   declarations: [
