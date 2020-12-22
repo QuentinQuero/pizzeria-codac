a11b78c4e9f15c9715beb8c7c8ffd077d057eb2b
Tony Bellec <tony1.bellec@epitech.eu>
2020-12-22 09:54:48 +0100

    Add component folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {APIInterceptor} from "./HttpInterceptor";
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { CartComponent } from './cart/cart.component';
 import { FormsModule }   from '@angular/forms';
-import { PizzaComponent } from './pizza/pizza.component';
+import { PizzaComponent } from './components/pizza/pizza.component';
 
 @NgModule({
   declarations: [
