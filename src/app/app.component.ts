704ac4b2b6a32e494a33992f5ee0201ad6086abc
Roma <roman.fechtchenko@epitech.eu>
2020-12-22 11:34:03 +0100

    add changes pizza.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pizzeria-codac';
-  pizza = {name: '4 fromages', base:'fromage', price:'8$',ingredients:['fromage1','fromage2','fromage3']}
+  pizza = {name: '4 Fromages', base:'Fromage', price:'8$',ingredients:['fromage1','fromage2','fromage3'], img:'https://commande.dominos.fr/ManagedAssets/FR/product/P4FR/FR_P4FR_fr_hero_8003.jpg?v-1416700018'}
 }
