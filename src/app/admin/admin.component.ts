e4bf749b11cb9f4417e237c9526246d1ede479f4
Quentin <quentin2.quero@epitech.eu>
2020-12-23 14:49:35 +0100

    change td list

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -20,9 +20,31 @@ export class AdminComponent implements OnInit {
         this.commandes = commandes;
       });
   }
-  modifyAccepted (f: NgForm) {
-    let idcommand = f.value.idcommand;
-    console.log(idcommand);
-    this._httpClient.put(`commandes/${idcommand}`,{ "accepted": true})
+
+  changeAccepted(isAccepted, id) {
+    console.log(isAccepted);
+    console.log(id);
+    this._httpClient.put(`commandes/${id}`,{ "accepted": isAccepted})
+      .subscribe(commande => {
+      console.log(commande);
+    });
+  }
+
+  changePayed(isPayed, id) {
+    console.log(isPayed);
+    console.log(id);
+    this._httpClient.put(`commandes/${id}`,{ "payed": isPayed})
+      .subscribe(commande => {
+      console.log(commande);
+    });
+  }
+
+  changeSent(isSent, id) {
+    console.log(isSent);
+    console.log(id);
+    this._httpClient.put(`commandes/${id}`,{ "sent": isSent})
+      .subscribe(commande => {
+        console.log(commande);
+    });
   }
 }
