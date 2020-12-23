c6e83b7069c5d26cbbb396139ecec3aff7f66579
Quentin <quentin2.quero@epitech.eu>
2020-12-23 16:21:16 +0100

    change modal for push

--- src/app/components/pizza/pizza.component.ts
+++ src/app/components/pizza/pizza.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit, Output} from '@angular/core';
 import {EventEmitter} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-pizza',
@@ -10,16 +11,40 @@ export class PizzaComponent implements OnInit {
 
   @Input() pizza: {name, base, price, ingredients:[{name}], image: {url}};
   @Input() sizes;
-  @Input() douth;
+  @Input() douths;
   @Output() pizzaEvent = new EventEmitter<Object>();
 
-  constructor() { }
+  numberPizzas = 1;
+  sizeSelected = null;
+  doughSelected = null;
+
+  constructor(private _httpClient: HttpClient) { }
 
   ngOnInit(): void {
   }
 
-  public onClick = pizza => {
-    this.pizzaEvent.emit(pizza);
+
+  public validatePizzaSelection () {
+    let currentSize = this.sizes.find(sizeSelected => sizeSelected.name == this.sizeSelected);
+    let currentDough = this.douths.find(doughSelected => doughSelected.name == this.doughSelected);
+
+    let objToSend = {
+      number: this.numberPizzas,
+      pizza: this.pizza,
+      dough: currentDough,
+      pizza_size: currentSize
+    }
+
+    // number: f.value.qty,
+    //   pizza: f.value.name,
+    //   dough: f.value.dough,
+    //   pizza_size: f.value.size
+
+    this._httpClient.post('pizza-createds', objToSend)
+      .subscribe(response => {
+        console.log(response);
+      });
+
   }
 
 }
